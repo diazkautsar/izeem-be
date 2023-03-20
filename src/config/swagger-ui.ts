23b4ae9d9e086e7b04c0af10206c06dfbe8a91bb
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-03-20 18:52:18 +0700

    config swagger-ui

---
+++ src/config/swagger-ui.ts
@@ -0,0 +1,25 @@
+import { FastifySwaggerUiOptions } from '@fastify/swagger-ui';
+
+const swaggerUiOptions: FastifySwaggerUiOptions = {
+    routePrefix: '/documentation',
+    uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false,
+    },
+    uiHooks: {
+        onRequest: function (request, reply, next) {
+            next();
+        },
+        preHandler: function (request, reply, next) {
+            next();
+        },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => {
+        return swaggerObject;
+    },
+    transformSpecificationClone: true,
+};
+
+export default swaggerUiOptions as FastifySwaggerUiOptions;
