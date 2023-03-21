7a4d95747842c15244413ef71354f5e188f8effb
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-03-21 13:14:56 +0700

    create constant

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const SLUG_SECTION_PERSONAL_INFORMATION = 'personal_information';
+export const SLUG_SECTION_PROFILE = 'profile';
+export const SLUG_SECTION_SKILLS = 'skills';
+export const SLUG_SECTION_EXPERIENCE = 'experience';
+export const SLUG_SECTION_EDUCATION = 'education';
+
+export const MPPING_DEFAULT_SLUG_SECTION_WITH_ORDER = {
+    [SLUG_SECTION_PERSONAL_INFORMATION]: 1,
+    [SLUG_SECTION_PROFILE]: 2,
+    [SLUG_SECTION_SKILLS]: 3,
+    [SLUG_SECTION_EXPERIENCE]: 4,
+    [SLUG_SECTION_EDUCATION]: 5,
+};
