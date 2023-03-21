66b9355991b10e99dc015947f97ba597d518b970
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-03-21 13:17:29 +0700

    add plugins

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import Autoload from '@fastify/autoload';
 import path from 'path';
 
 import * as config from './config';
+import plugins from './plugins';
 
 const App = async () => {
     const instance = fastify({
@@ -12,11 +13,22 @@ const App = async () => {
     });
 
     instance.decorate('config', config);
+    instance.register(plugins);
 
     instance.register(Autoload, {
         dir: path.resolve(__dirname, 'routes'),
         ignorePattern: /.*.(test|spec|entity).(js|ts)/,
         maxDepth: 1,
+    });
+
+    instance.setErrorHandler((error, request, reply) => {
+        reply.status(Number(error.statusCode ?? 500)).send({
+            status: false,
+            statusCode: error.code,
+            message: error.message,
+            messageTitle: error.message,
+            data: null,
+        });
     });
 
     return instance;
