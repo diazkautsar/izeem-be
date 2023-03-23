d8aa6680a4d11e9447fb77c57e0b5c3347080561
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-03-23 11:21:45 +0700

    add some constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,10 +4,32 @@ export const SLUG_SECTION_SKILLS = 'skills';
 export const SLUG_SECTION_EXPERIENCE = 'experience';
 export const SLUG_SECTION_EDUCATION = 'education';
 
+export const SLUG_FIELD_PHONE = 'phone';
+export const SLUG_FIELD_EMAIL = 'email';
+export const SLUG_FIELD_ADDRESS = 'address';
+export const SLUG_FIELD_WEBSITE = 'website';
+export const SLUG_FIELD_SKILL = 'skills';
+export const SLUG_FIELD_POSITION = 'position';
+export const SLUG_FIELD_START_DATE = 'start_date';
+export const SLUG_FIELD_END_DATE = 'end_date';
+export const SLUG_FIELD_DESCRIPTION = 'description';
+export const SLUG_FIELD_SCHOOL_NAME = 'school_name';
+export const SLUG_FIELD_DEGREE = 'degree';
+export const SLUG_FIELD_FIELD = 'field';
+
 export const MPPING_DEFAULT_SLUG_SECTION_WITH_ORDER = {
     [SLUG_SECTION_PERSONAL_INFORMATION]: 1,
     [SLUG_SECTION_PROFILE]: 2,
     [SLUG_SECTION_SKILLS]: 3,
     [SLUG_SECTION_EXPERIENCE]: 4,
     [SLUG_SECTION_EDUCATION]: 5,
+};
+
+export const SLUG_PERSONAL_INFORMATION = [SLUG_FIELD_PHONE, SLUG_FIELD_EMAIL, SLUG_FIELD_ADDRESS, SLUG_FIELD_WEBSITE];
+
+export const MAPPING_DEFAULT_ORDER_USER_FIELD_PERSONAL_INFORMATION = {
+    [SLUG_FIELD_PHONE]: 1,
+    [SLUG_FIELD_EMAIL]: 2,
+    [SLUG_FIELD_ADDRESS]: 3,
+    [SLUG_FIELD_WEBSITE]: 4,
 };
