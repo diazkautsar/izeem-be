1a9ee501a32e1d3983ab656862d5269ec509a873
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-03-25 22:03:58 +0700

    controllers get user personal information

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,4 @@
-export { createUserPersonalInformationController } from './userPersonalInformation';
+export {
+    createUserPersonalInformationController,
+    getUserPersonalInformationController,
+} from './userPersonalInformation';
--- src/controllers/userPersonalInformation/index.ts
+++ src/controllers/userPersonalInformation/index.ts
@@ -1,1 +1,2 @@
 export { default as createUserPersonalInformationController } from './createUserPersonalInformation.controller';
+export { default as getUserPersonalInformationController } from './getUserPersonalInformation.controller';
